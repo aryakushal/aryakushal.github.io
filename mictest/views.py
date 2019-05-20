26c4229e060e63aaaf4ab039b0989358e1e9e2a6
root <root@YY136856.code1.emi.philips.com>
2019-05-21 01:27:29 +0530

    adding template

--- mictest/views.py
+++ mictest/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-        return HttpResponse("Dang! Working.")
+    #return HttpResponse("Dang! Working.")
+    context = { "theNorth" : [ { "id" : 1, "name" : "John Snow" }, { "id" : 2, "name" : "Daenarys Targeryn" } ] }
+    template = loader.get_template('mictest/index.html')
+    return HttpResponse( template.render(context, request) )
 
 def detail(request, question_id):
     return HttpResponse("You are looking at question %s." % question_id)
