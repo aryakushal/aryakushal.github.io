bb955de5f01634573fa2bc25a1e74163dc45fb0e
root <root@YY136856.code1.emi.philips.com>
2019-05-21 00:10:29 +0530

    fixing routing issue

--- hubble/urls.py
+++ hubble/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mic-test', include('mictest.urls')),
+    path('mic-test/', include('mictest.urls')),
 
 ]
